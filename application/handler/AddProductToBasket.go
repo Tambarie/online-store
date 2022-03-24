6bec595485768ac2c79fb4e5486a5a1d37ba7657
tambarie <gt.gbaragbo@gmail.com>
2022-03-24 14:18:56 +0100

    add AddProductToBasket method

--- application/handler/AddProductToBasket.go
+++ application/handler/AddProductToBasket.go
@@ -3,20 +3,29 @@ package handler
 import (
 	"github.com/Tambarie/online-store/application/helpers"
 	domain "github.com/Tambarie/online-store/domain/store"
+	"github.com/Tambarie/online-store/response"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 )
 
 func (h *Handler) AddProductToBasket() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		productID := context.Query("product_id")
+
 		basket := &domain.Basket{}
 		basket.ProductID = productID
 
-		if err := helpers.Decode(context, basket); err != nil {
-			log.Fatalf("Error  cannot bind %v", err)
+		if err := helpers.Decode(context, &basket); err != nil {
+			log.Fatalf("Error %v", err)
 			return
 		}
 
+		_, err := h.StoreService.AddProductToBasket(basket)
+		if err != nil {
+			context.AbortWithStatusJSON(500, err)
+			return
+		}
+		response.JSON(context, http.StatusCreated, nil, nil, "product added successfully")
 	}
 }
