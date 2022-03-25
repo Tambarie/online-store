9dba76c17e9cd8c0ec10fb1472f6206b72f8bab1
tambarie <gt.gbaragbo@gmail.com>
2022-03-25 19:22:25 +0100

    Add comments

--- application/handler/UpdateProducts.go
+++ application/handler/UpdateProducts.go
@@ -11,20 +11,25 @@ import (
 
 func (h *Handler) UpdateProducts() gin.HandlerFunc {
 	return func(context *gin.Context) {
+		// Logging the context
+		helpers.LogRequest(context)
 
 		productID := context.Param("product_id")
 		product := &domain.Products{}
 		product.UpdatedAt = time.Now()
 
+		// Binds the JSON
 		if err := helpers.Decode(context, &product); err != nil {
 			context.AbortWithStatusJSON(http.StatusBadRequest, err)
 			return
 		}
+		// Updates the product by it's ID
 		err := h.StoreService.UpdateProducts(productID, product)
 		if err != nil {
 			context.AbortWithStatusJSON(http.StatusInternalServerError, err)
 		}
 
+		// JSON Response to the client
 		response.JSON(context, http.StatusCreated, nil, nil, "product updated successfully")
 	}
 }
