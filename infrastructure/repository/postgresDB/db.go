ab52c5ccc1e7e2d51df0419e958f55a1a89e2edb
tambarie <gt.gbaragbo@gmail.com>
2022-03-25 16:11:05 +0100

    change env file names

--- infrastructure/repository/postgresDB/db.go
+++ infrastructure/repository/postgresDB/db.go
@@ -15,8 +15,7 @@ func Init() *gorm.DB {
 	dsn = os.Getenv("DATABASE_URL")
 
 	if dsn == "" {
-		dsn = fmt.Sprintf("host=%v user=%v dbname=%v port=%v sslmode=%v TimeZone=%v", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"),
-			os.Getenv("DB_PORT"), os.Getenv("DB_SSLMODE"), os.Getenv("TIMEZONE"))
+		dsn = fmt.Sprintf("host=%s port=%v user=%s password=%s dbname=%s sslmode=disable", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
 	}
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
