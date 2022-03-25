e458525de12e13c4ba1e1ce70efeec9e310d4f6d
tambarie <gt.gbaragbo@gmail.com>
2022-03-25 16:12:22 +0100

    added initializelogs

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/Tambarie/online-store/application/helpers"
 	"github.com/Tambarie/online-store/application/server"
 	"github.com/joho/godotenv"
 	"log"
 )
 
 func main() {
+	helpers.InitializeLogDir()
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Error loading .env with godotenv: %s", err)
