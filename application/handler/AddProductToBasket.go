d07af3125c80088bcb1d67c38ce42cf8b3c21cf5
tambarie <gt.gbaragbo@gmail.com>
2022-03-25 19:21:19 +0100

    Add comments

--- application/handler/AddProductToBasket.go
+++ application/handler/AddProductToBasket.go
@@ -9,23 +9,32 @@ import (
 	"net/http"
 )
 
+// AddProductToBasket Handles the addition of products to the basket
 func (h *Handler) AddProductToBasket() gin.HandlerFunc {
 	return func(context *gin.Context) {
+		// Logging the context
+		helpers.LogRequest(context)
+
+		// Getting the productID from the query string
 		productID := context.Query("product_id")
 
 		basket := &domain.Basket{}
 		basket.ProductID = productID
 
+		// Binding the json
 		if err := helpers.Decode(context, &basket); err != nil {
 			log.Fatalf("Error %v", err)
 			return
 		}
 
+		// saving products to basket
 		_, err := h.StoreService.AddProductToBasket(basket)
 		if err != nil {
 			context.AbortWithStatusJSON(500, err)
 			return
 		}
+
+		// JSON Response to the client
 		response.JSON(context, http.StatusCreated, nil, nil, "product added successfully")
 	}
 }
