d350d5ca1be4c1f1ace3d0f664a64f7059ed265a
tambarie <gt.gbaragbo@gmail.com>
2022-03-25 19:21:42 +0100

    Add comments

--- application/handler/DeleteProduct.go
+++ application/handler/DeleteProduct.go
@@ -11,23 +11,29 @@ import (
 
 func (h *Handler) DeleteProduct() gin.HandlerFunc {
 	return func(context *gin.Context) {
+		// Logging the context
+		helpers.LogRequest(context)
+
 		product := struct {
 			ProductID string `json:"product_id"`
 		}{}
 
-		production := &domain.Products{}
+		products := &domain.Products{}
 
+		// Binds the JSON
 		if err := helpers.Decode(context, &product); err != nil {
 			log.Fatalf("Error  cannot bind %v", err)
 			return
 		}
 
-		err := h.StoreService.DeleteProduct(product.ProductID, production)
+		// Deletes product from the database
+		err := h.StoreService.DeleteProduct(product.ProductID, products)
 		if err != nil {
 			context.AbortWithStatusJSON(500, err)
 			return
 		}
 
+		// JSON Response to the client
 		response.JSON(context, http.StatusOK, nil, nil, "product delete successfully")
 
 	}
