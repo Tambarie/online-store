6d89617b0a38c6d04e4993353f80bb776ff6f6a1
tambarie <gt.gbaragbo@gmail.com>
2022-03-25 19:22:09 +0100

    Add comments

--- application/handler/ListAllProducts.go
+++ application/handler/ListAllProducts.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/Tambarie/online-store/application/helpers"
 	"github.com/Tambarie/online-store/response"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -8,12 +9,16 @@ import (
 
 func (h *Handler) ListAllProducts() gin.HandlerFunc {
 	return func(context *gin.Context) {
+		// Logging the context
+		helpers.LogRequest(context)
 
+		// List all products in the database
 		allProducts, err := h.StoreService.ListAllProducts()
 		if err != nil {
 			context.AbortWithStatusJSON(500, err)
 			return
 		}
+		// JSON Response to the client
 		response.JSON(context, http.StatusCreated, allProducts, nil, "all products")
 	}
 }
