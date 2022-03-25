deba6a9cbbabfaaaf08d48dd8859df35ba0bf905
tambarie <gt.gbaragbo@gmail.com>
2022-03-25 19:21:59 +0100

    Add comments

--- application/handler/GetProductDetails.go
+++ application/handler/GetProductDetails.go
@@ -10,23 +10,33 @@ import (
 
 func (h *Handler) GetProductDetails() gin.HandlerFunc {
 	return func(context *gin.Context) {
+
+		// Logging the context
+		helpers.LogRequest(context)
+
 		product := struct {
 			ProductID string `json:"product_id"`
 		}{}
 
+		// Binding the json
 		if err := helpers.Decode(context, &product); err != nil {
 			log.Fatalf("Error %v", err)
 			return
 		}
+
+		// Gets the product by it's ID from the database
 		details, err := h.StoreService.GetProductDetails(product.ProductID)
 		if err != nil {
 			context.AbortWithStatusJSON(http.StatusBadRequest, err)
 			return
 		}
+
+		// Checks if the productID is valid
 		if product.ProductID != details.ProductID {
 			response.JSON(context, http.StatusBadRequest, nil, nil, "please enter a valid product id")
 			return
 		}
+		// JSON Response to the client
 		response.JSON(context, http.StatusCreated, details, nil, "products fetched successfully")
 	}
 }
